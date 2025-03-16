c4e238785ef4aed178cc4dd18da5f009262e53da
marcos <166825415+marcosbarrosj@users.noreply.github.com>
2025-03-16 10:49:07 -0300

    atualizacao

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,28 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+//função numerica
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach ((valueDisplay) =>{
+   let starValue = 0;
+   let endValue = parseInt(valueDisplay.getAttribute("data-val"))
+     let duration = Math.floor(interval / endValue);
+     let counter = setInterval(function(){
+       starValue += 1;
+       valueDisplay.textContent = starValue;
+       if (starValue == endValue) {
+             clearInterval(counter)
+       }
+
+
+     },duration);
+
+});
+
+
+
 
 (function() {
   "use strict";
